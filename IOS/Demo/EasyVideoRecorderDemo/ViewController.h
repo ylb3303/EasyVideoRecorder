2b73d5b751f06c46ca314bbd4676221fca974472
Babosa <mysunpany@gmail.com>
2016-03-08 00:04:41 +0800

    EasyVideoRecorder IOS Demo

---
+++ IOS/Demo/EasyVideoRecorderDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasyVideoRecorderDemo
+//
+//  Created by chenshun on 16/3/4.
+//  Copyright © 2016年 EasyDarwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
