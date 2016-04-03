3f7d6a9ad80dfd565dcab191e4e2cc0eb44de6ad
Babosa <mysunpany@gmail.com>
2016-04-03 12:29:31 +0800

    remove old ios lib

--- IOS/EasyVideoRecorderLib/EasyCaptureType.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef _EasyCaptureType_h
-#define _EasyCaptureType_h
-
-#define kErrorExceedMaxSegments   -10
-
-typedef void(^EasyVideoOutputCompletion)(NSString *exportPath, NSInteger error);
-
-#endif
